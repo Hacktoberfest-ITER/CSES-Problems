734e3897e371553c415f9473731799ac7653f4ca
Saniya Ahuja <ahuja.saniya@gmail.com>
2020-10-01 11:08:54 +0530

    Added my solution

---
+++ python/saniya3/saniya3_missing_number.py
@@ -0,0 +1,14 @@
+# Missing number
+# https://cses.fi/problemset/task/1083/
+
+n=int(input())
+y=[int(i) for i in input().split()]
+
+def missing(seq,num):
+    n=len(seq)+1
+    total=n*(n+1)/2
+    print(int(total-sum(seq)))
+    
+missing(y,n)
+
+# author: saniya3
