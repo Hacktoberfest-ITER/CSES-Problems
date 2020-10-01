5b87d7deede74d6373ea0c8c39d7fcb2c2ca87a4
Abhishek Kumar <abhisonuk123@gmail.com>
2020-10-01 14:12:38 +0530

    added distinct_numbers solution

---
+++ python/iamAbhishekkumar/iamAbhishekkumar_distinct_numbers.py
@@ -0,0 +1,10 @@
+# user id :
+# https://cses.fi/user/32366
+
+# You are given a list of n integers, and your task is to calculate the number of distinct values in the list.
+# https://cses.fi/problemset/task/1621/
+# author : iamAbhishekkumar
+
+n = input()
+m = list(input().split())
+print(len(set(m)))
