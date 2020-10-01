4b3dd24e82b0de61514a9efae26a7a253ed2aa9f
[Ravi Prasad] <[ravikrp22@gmail.com]>
2020-10-01 21:47:20 +0530

    Repetition solution in java

---
+++ java/RaviKumarPrasad/Raviruler_Repetition.java
@@ -0,0 +1,34 @@
+/*You are given a DNA sequence: a string consisting of characters A, C, G, and T. Your task is to find the longest repetition in the sequence. This is a maximum-length substring containing only one type of character.*/
+/*Example
+
+Input:
+ATTCGGGA
+
+Output:
+3
+
+link: https://cses.fi/problemset/task/1069
+*/
+import java.util.Scanner;
+public class Raviruler_Repetition {
+	public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	String s=scan.nextLine().toLowerCase();
+	int a=s.length(),b=1,c=1;
+	for (int i=1;i<a;i++)
+	{
+		if(s.charAt(i)== s.charAt(i-1))
+		{
+			c++;
+			b=Math.max(b,c);
+		}
+		else
+			c=1;
+	}
+	System.out.print(b);
+	}
+
+}
+//Author: Ravi Kumar Prasad
+//Git_hub Id: Raviruler
+//cses : https://cses.fi/user/32356/
