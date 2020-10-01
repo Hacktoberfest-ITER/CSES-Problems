3e583cc369ba7605283508883d07690fc287849f
A S V K Vinayak <asvkvinayak@gmail.com>
2020-10-01 21:59:34 +0530

    Added Questions in the top

--- c_plus_plus/ASVKVINAYAK/ASVKVINAYAK_WeirdAlgorithm.cpp
+++ c_plus_plus/ASVKVINAYAK/ASVKVINAYAK_WeirdAlgorithm.cpp
@@ -1,5 +1,14 @@
-// Weird algorithm
-// https://cses.fi/problemset/task/1068
+/*
+Weird algorithm
+Consider an algorithm that takes as input a positive integer n. If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. The algorithm repeats this, until n is one. For example, the sequence for n=3 is as follows:
+3?10?5?16?8?4?2?1
+
+Your task is to simulate the execution of the algorithm for a given value of n.
+
+ Question link : https://cses.fi/problemset/task/1068
+ Profile Link :https://cses.fi/user/32376 
+ Author: ASVKVINAYAK
+*/
 
 #include<iostream>
 using namespace std;
@@ -34,5 +43,3 @@ int main()
 	}
     return 0;
 }
-
-// author: ASVKVINAYAK
