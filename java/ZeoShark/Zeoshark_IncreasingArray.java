4614ab9954d29ca3aeeca8e5092e7f2fff714f25
saurav <sauravberaiit@gmail.com>
2020-10-01 17:11:02 +0530

    added Increasing Array

---
+++ java/ZeoShark/Zeoshark_IncreasingArray.java
@@ -0,0 +1,33 @@
+//Increasing Array
+//You are given an array of n integers. You want to modify the array so that it is increasing, i.e., every element is at least as large as the previous element.
+
+//On each turn, you may increase the value of any element by one. What is the minimum number of turns required?
+//Question: https://cses.fi/problemset/task/1094/
+
+//Solution: https://cses.fi/problemset/result/1067056/
+
+//User Id: https://cses.fi/user/32264
+
+//author: souravbera
+import java.util.*;
+public class Zeoshark_IncreasingArray
+{
+    public static void main(String args[])
+    {   
+        long minTurns=0;
+        Scanner in= new Scanner(System.in);
+        int n= in.nextInt();
+        int a[]= new int[n];
+        a[0]= in.nextInt();
+        for(int i=1;i<n;i++)
+        {
+            a[i]=in.nextInt();
+            if(a[i-1]>a[i])
+            {
+                minTurns+=(a[i-1]-a[i]);
+                a[i]= a[i-1];
+            }
+        }
+        System.out.println(minTurns);
+    }
+}
