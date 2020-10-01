2c11bfe327a98d72858efdfafc723112e348c75d
Sayantan Paul <belikesayantan12@gmail.com>
2020-10-01 19:43:27 +0530

    Sayantan Paul - Subarray Sum 1

---
+++ python/belikesayantan/belikesayantan_subarray_sum.py
@@ -0,0 +1,22 @@
+# author: https://cses.fi/user/32392
+# @belikesayantan
+
+
+'''Subarray Sums I : https://cses.fi/problemset/task/1660/'''
+from collections import defaultdict
+
+_, x = (int(x) for x in input().split())
+arr = [int(x) for x in input().split()]
+
+count, curr_sum = 0, 0
+sums_count = defaultdict(int)
+
+for i in arr:
+    curr_sum += i
+    if curr_sum == x:
+        count += 1
+    if curr_sum - x in sums_count:
+        count += sums_count[curr_sum - x]
+    sums_count[curr_sum] += 1
+
+print(count)
