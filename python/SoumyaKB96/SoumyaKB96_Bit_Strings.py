9da765a789c5610bf8c605a77828efac980ed0be
Soumyakanta Boity <42297139+SoumyaKB96@users.noreply.github.com>
2020-10-01 18:24:39 +0530

    Added Bit_String

---
+++ python/SoumyaKB96/SoumyaKB96_Bit_Strings.py
@@ -0,0 +1,13 @@
+#Q.  Your task is to calculate the number of bit strings of length n.
+#Qn Link - https://cses.fi/problemset/task/1617/
+
+a=int(input())
+
+print((2**a)%(10**9 + 7))
+
+
+
+
+
+# Author : SoumyaKB96
+# result - https://cses.fi/problemset/result/1065192/
