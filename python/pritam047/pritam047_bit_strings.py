7d995e6603f63acc2adb956db12e12ad416e46af
pritam047 <pritamkar13@gmail.com>
2020-10-02 00:46:43 +0530

    creating_strings_1.py added

--- python/pritam047/pritam047_bit_strings.py
+++ python/pritam047/pritam047_bit_strings.py
@@ -2,5 +2,8 @@
 # https://cses.fi/user/32269
 # PROBLEM NUMBER
 # https://cses.fi/problemset/task/1617/
+# Your task is to calculate the number of bit strings of length n.
+# QUESTION
+# For example, if n=3, the correct answer is 8, because the possible bit strings are 000, 001, 010, 011, 100, 101, 110, and 111.
 n= int(input())
 print((2**n) % ((10**9)+7))
