ca373cd9f11c177582938fbcafde5c56f9c63894
Ayush Kejariwal <kejayush29@gmail.com>
2020-10-02 14:49:36 +0530

    added comments

--- java/KejariwalAyush/kejariwalAyush_TrailingZerors.java
+++ java/KejariwalAyush/kejariwalAyush_TrailingZerors.java
@@ -1,3 +1,10 @@
+// Your task is to calculate the number of trailing zeros in the factorial n!.
+
+// For example, 20!=2432902008176640000 and it has 4 trailing zeros.
+//https://cses.fi/problemset/task/1618/ 
+
+//author: KejariwalAyush
+
 import java.util.Scanner;
 
 public class kejariwalAyush_TrailingZerors {
