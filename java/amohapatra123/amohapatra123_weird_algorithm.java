8aa4bfede5db029318e41002ab4ff8df66f9ed38
Anshuman Mohapatra <amohapatra2001@gmail.com>
2020-10-02 12:38:49 +0530

    question info added

--- java/amohapatra123/amohapatra123_weird_algorithm.java
+++ java/amohapatra123/amohapatra123_weird_algorithm.java
@@ -1,3 +1,15 @@
+/* Consider an algorithm that takes as input a positive integer n. If n is even,
+ the algorithm divides it by two, and if n is odd, the algorithm multiplies 
+ it by three and adds one. The algorithm repeats this, until n is one.
+ For example, the sequence for n=3 is as follows:
+ 3→10→5→16→8→4→2→1
+ */
+ 
+//https://cses.fi/problemset/task/1068/
+
+//author:amohapatra123
+
+
 import java.util.Scanner;
 public class amohapatra123_weird_algorithm{
 
