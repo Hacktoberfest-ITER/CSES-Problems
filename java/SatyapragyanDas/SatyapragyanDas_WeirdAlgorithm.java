43acf37df39c6abae8a61c3a47714c48898f92e8
SatyapragyanDas <53405743+SatyapragyanDas@users.noreply.github.com>
2020-10-04 01:31:29 +0530

    Update SatyapragyanDas_WeirdAlgorithm.java
    Added credentials

--- java/SatyapragyanDas/SatyapragyanDas_WeirdAlgorithm.java
+++ java/SatyapragyanDas/SatyapragyanDas_WeirdAlgorithm.java
@@ -1,3 +1,10 @@
+/**Consider an algorithm that takes as input a positive integer n. If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. The algorithm repeats this, until n is one. For example, the sequence for n=3 is as follows:
+3→10→5→16→8→4→2→1
+
+Your task is to simulate the execution of the algorithm for a given value of n. */
+
+// Question Link: https://cses.fi/problemset/task/1068
+
 import java.util.Scanner;
 
 public class WeirdAlgorithm {
@@ -20,3 +27,6 @@ public class WeirdAlgorithm {
 		}
 	}
 }
+
+//author: SatyapragyanDas
+//profile: https://cses.fi/user/32452
