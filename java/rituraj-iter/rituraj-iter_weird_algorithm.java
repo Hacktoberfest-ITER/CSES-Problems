9a3c290f6442711139db5715eab35abbdb6d04d0
Rituraj Gupta <rituraj.iter@gmail.com>
2020-10-04 15:01:17 +0530

    Update rituraj-iter_weird_algorithm.java

--- java/rituraj-iter/rituraj-iter_weird_algorithm.java
+++ java/rituraj-iter/rituraj-iter_weird_algorithm.java
@@ -1,3 +1,7 @@
+/*Consider an algorithm that takes as input a positive integer n. If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. The algorithm repeats this, until n is one. For example, the sequence for n=3 is as follows:
+3→10→5→16→8→4→2→1
+Your task is to simulate the execution of the algorithm for a given value of n.*/
+// https://cses.fi/problemset/task/1068
 import java.io.*;
 public class weird_algorithm{
 	public static void main(String args[]) throws NumberFormatException, IOException {
@@ -15,3 +19,5 @@ public class weird_algorithm{
         System.out.println(sb);
 	}
 }
+// author: rituraj-iter
+// profile: https://cses.fi/user/32401
