1f014ac8accf8315816938a9f13d15195824e271
[Akhilesh Mattam] <[akhileshmattam782@gmail.com]>
2020-10-04 13:57:17 +0530

    Added trailing zero solution.cpp

--- c_plus_plus/AkhileshMattam/triling_zeros.cpp
+++ c_plus_plus/AkhileshMattam/triling_zeros.cpp
@@ -1,3 +1,8 @@
+//Your task is to calculate the number of trailing zeros in the factorial n!.
+//For example, 20!=2432902008176640000 and it has 4 trailing zeros.
+//program link:https://cses.fi/problemset/task/1618
+//Author:AkhileshMattam
+//profile link:https://cses.fi/user/32346
 #include <iostream>
 using namespace std;
 int main()
