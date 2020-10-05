449b6743aa1f30a5a47bbe449914422722ffc89c
Saniya Ahuja <53290877+saniya3@users.noreply.github.com>
2020-10-05 10:46:02 +0530

    Update saniya3_weird_algorithm.py
    added question

--- python/saniya3/saniya3_weird_algorithm.py
+++ python/saniya3/saniya3_weird_algorithm.py
@@ -1,4 +1,7 @@
-# Weird algorithm
+# Weird algorithm:
+#Consider an algorithm that takes as input a positive integer n. 
+# If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. 
+# The algorithm repeats this, until n is one. 
 # https://cses.fi/problemset/task/1068/
 
 x=int(input())
