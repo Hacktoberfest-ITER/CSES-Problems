b80356b1b2d4a564be5ec0523d054997e94589d9
Mayank Sahu <53302216+DE-L-TA@users.noreply.github.com>
2020-10-05 22:29:50 +0530

    Add files via upload

---
+++ DE-L-TA/de-l-ta__increasing_array.py
@@ -0,0 +1,24 @@
+#link-checklist-https://cses.fi/problemset/result/1086177/
+#Author-Mayank Sahu
+#You are given an array of n integers. You want to modify the array so that it is increasing, i.e., every element is at least as large as the previous element.
+
+#On each turn, you may increase the value of any element by one. What is the minimum number of turns required?
+
+i=input
+t=i()
+n,c,d=0,1,1
+ 
+ 
+for i in i().split():
+    i=int(i)
+    d+=1 
+    if c>i:
+        n+=c-i
+    else:
+        c=i
+if d==int(t):
+  print(n)
+else:
+  exit(0)
+  
+  
