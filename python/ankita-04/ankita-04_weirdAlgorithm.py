873d5ba9ccf8fd6f4a1781f2f8681fafd802e08c
Ankita Satapathy <45062304+ankita-04@users.noreply.github.com>
2020-10-06 10:13:21 +0530

    Updated weirdAlgorithm problem

--- python/ankita-04/ankita-04_weirdAlgorithm.py
+++ python/ankita-04/ankita-04_weirdAlgorithm.py
@@ -1,3 +1,4 @@
+# Author: Ankita Satapathy
 #Consider an algorithm that takes as input a positive integer n. 
 # If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. 
 # The algorithm repeats this, until n is one. 
