b8676a97f67acec2f89569069cc416963605bae1
Rishika Wadhera <rishika.wadhera6@gmail.com>
2020-10-07 10:23:00 +0530

    increasing array added

---
+++ java/Rishika6/Rishika6_IncreasingArray.java
@@ -0,0 +1,33 @@
+/*You are given an array of n integers. You want to modify the array so that it is increasing, i.e., every element is at least as large as the previous element.
+On each turn, you may increase the value of any element by one. What is the minimum number of turns required?*/
+//https://cses.fi/problemset/task/1094/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class IncreasingArray {
+
+	public static void main(String[] args) {
+	/*	//FastReader fr=new FastReader();
+	//	int n=fr.nextInt();
+		//int a[]=new int[n];
+		//for(int i=0;i<n;i++) {
+			//a[i]=fr.nextInt();
+		}
+		long turns=0;
+		for(int i=0;i<n-1;i++) {
+			if(a[i]>a[i+1]) {
+				turns+=Math.abs(a[i]-a[i+1]);
+				a[i+1]=a[i];
+			}
+		}
+		System.out.println(turns);*/
+		
+
+	}
+
+}
+
+//author:Rishika
+//https://cses.fi/user/32327
