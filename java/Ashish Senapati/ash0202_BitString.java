665b16f2317b1c04d49897bc3c62f59defe85808
Ashish Kumar Senapati <53336453+ash0202@users.noreply.github.com>
2020-10-11 23:32:38 +0530

    Update ash0202_BitString.java

--- java/Ashish Senapati/ash0202_BitString.java
+++ java/Ashish Senapati/ash0202_BitString.java
@@ -1,3 +1,30 @@
+/*
+profile-https://cses.fi/user/32749
+link-https://cses.fi/problemset/task/1617/
+
+Your task is to calculate the number of bit strings of length n.
+
+For example, if n=3, the correct answer is 8, because the possible bit strings are 000, 001, 010, 011, 100, 101, 110, and 111.
+
+Input
+
+The only input line has an integer n.
+
+Output
+
+Print the result modulo 109+7.
+
+Constraints
+1≤n≤106
+Example
+
+Input:
+3
+
+Output:
+8
+
+*/
 import java.math.BigInteger;
 import java.util.Scanner;
 
