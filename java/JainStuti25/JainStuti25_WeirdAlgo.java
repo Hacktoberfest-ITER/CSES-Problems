239afe784605206d876cfee3492a09509345ea7f
Stuti Jain <41702974+JainStuti25@users.noreply.github.com>
2020-10-16 22:31:09 +0530

    Update and rename java/StutiJain/Stuti_WeirdAlgo.java to java/JainStuti25/JainStuti25_WeirdAlgo.java

--- java/JainStuti25/JainStuti25_WeirdAlgo.java
+++ java/JainStuti25/JainStuti25_WeirdAlgo.java
@@ -4,9 +4,12 @@
 // https://cses.fi/user/25504
 // PROBLEM NUMBER
 // https://cses.fi/problemset/task/1068
-// Weird Algorithm
+// Consider an algorithm that takes as input a positive integer n. If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. The algorithm repeats this, until n is one. For example, the sequence for n=3 is as follows:
+//3→10→5→16→8→4→2→1
+//Your task is to simulate the execution of the algorithm for a given value of n.
 
-public class Stuti_WeirdAlgo {
+
+public class JainStuti25_WeirdAlgo {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
