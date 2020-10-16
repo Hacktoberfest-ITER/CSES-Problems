625d4d7f3cd49bde0c86599b648020b498cd4d9f
Stuti Jain <41702974+JainStuti25@users.noreply.github.com>
2020-10-16 22:27:23 +0530

    Update and rename java/StutiJain/Stuti_IncreasingArr.java to java/JainStuti25/JainStuti25_IncreasingArr.java

--- java/JainStuti25/JainStuti25_IncreasingArr.java
+++ java/JainStuti25/JainStuti25_IncreasingArr.java
@@ -4,9 +4,10 @@
 // https://cses.fi/user/25504
 // PROBLEM NUMBER
 // https://cses.fi/problemset/task/1094
-// Increasing Array
+// You are given an array of n integers. You want to modify the array so that it is increasing, i.e., every element is at least as large as the previous element.
+//On each turn, you may increase the value of any element by one. What is the minimum number of turns required?
 
-public class Stuti_IncreasingArr {
+public class JainStuti25_IncreasingArr {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
