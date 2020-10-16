08482bb529d351bf2b3c99ba0fa77ad3063e8ab5
Stuti Jain <41702974+JainStuti25@users.noreply.github.com>
2020-10-16 22:28:21 +0530

    Update and rename java/StutiJain/Stuti_MissingNum.java to java/JainStuti25/JainStuti25_MissingNum.java

--- java/JainStuti25/JainStuti25_MissingNum.java
+++ java/JainStuti25/JainStuti25_MissingNum.java
@@ -4,9 +4,9 @@
 // https://cses.fi/user/25504
 // PROBLEM NUMBER
 // https://cses.fi/problemset/task/1083
-// Missing Number
+// You are given all numbers between 1,2,…,n except one. Your task is to find the missing number.
 
-public class Stuti_MissingNum {
+public class JainStuti25_MissingNum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long n = sc.nextInt();
