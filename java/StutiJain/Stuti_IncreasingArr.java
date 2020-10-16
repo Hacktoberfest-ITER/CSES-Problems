17da1c87dfaaf741d0d2d8b3ea608f7b07b4c64c
Stuti Jain <41702974+JainStuti25@users.noreply.github.com>
2020-10-16 18:40:25 +0530

    Create Stuti_IncreasingArr.java

---
+++ java/StutiJain/Stuti_IncreasingArr.java
@@ -0,0 +1,28 @@
+// AUTHOR
+// Stuti Jain
+// USER ID
+// https://cses.fi/user/25504
+// PROBLEM NUMBER
+// https://cses.fi/problemset/task/1094
+
+public class Stuti_IncreasingArr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long ans=0;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0;i<n-1;i++){
+            if(arr[i+1]<arr[i]){
+                ans=ans+(arr[i]-arr[i+1]);
+                arr[i+1]=arr[i];
+
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
